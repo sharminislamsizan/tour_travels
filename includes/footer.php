4a4c753b59c6eb2716267da43ed7480205c5210b
sharminislamsizan <sharminsizan@gmail.com>
2021-07-18 21:10:40 +0600

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+	
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+		
+	</div>
+</div>
